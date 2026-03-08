d1f211edee89c32fc23e338276844bbbb049880b
theetruej <williamsjn97@gmail.com>
2026-03-08 11:19:07 -0500

    hi

--- .next/static/chunks/app_layout_tsx_0a548d63._.js
+++ .next/static/chunks/app_layout_tsx_0a548d63._.js
@@ -1,7 +1,7 @@
 (globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/[root-of-the-server]__cbe26723._.css",
+  "static/chunks/[next]_internal_font_google_fa8a0cca._.css",
   "static/chunks/node_modules_c4817f3b._.js",
   "static/chunks/_1cf1f135._.js"
 ],
