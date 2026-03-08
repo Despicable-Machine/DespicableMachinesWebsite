fdd9ded84bc2c6ddcdfeb2a4cf564b653be194ff
theetruej <williamsjn97@gmail.com>
2026-03-08 11:40:17 -0500

    fixed bug

--- .next/static/chunks/app_layout_tsx_0a548d63._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[next]_internal_font_google_fa8a0cca._.css",
-  "static/chunks/node_modules_c4817f3b._.js",
-  "static/chunks/_1cf1f135._.js"
-],
-    source: "dynamic"
-});
